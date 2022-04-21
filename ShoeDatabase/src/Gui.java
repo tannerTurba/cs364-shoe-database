e7f9ffdf8b90c96a228e0365c72b49f44df889ef
becschoch <100818808+becschoch@users.noreply.github.com>
2022-04-21 14:44:33 -0500

    gui changes

--- ShoeDatabase/src/Gui.java
+++ ShoeDatabase/src/Gui.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.awt.image.*;
@@ -7,7 +8,7 @@ import java.io.*;
 
 import javax.imageio.*;
 
-public class Gui extends JFrame {
+public class Gui extends JFrame{
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     double screenWidth = screenSize.getWidth();
     double screenHeight = screenSize.getHeight();
@@ -26,7 +27,62 @@ public class Gui extends JFrame {
         String testShoe = "https://images.stockx.com/images/Nike-Air-Max-97-White-Wolf-Grey-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1607044794";
         // obj.displayShoeImage(testShoe);
         obj.setVisible(true);
+    
     }
+
+
+    public class RadioButtonDemo extends JPanel implements ActionListener {
+
+    JLabel picture;
+ 
+    public RadioButtonDemo() {
+        super(new BorderLayout());
+    //Create the radio buttons.
+
+    JRadioButton gucci = new JRadioButton("Gucci");
+    gucci.setMnemonic(KeyEvent.VK_B);
+    gucci.setActionCommand("Gucci");
+    gucci.setSelected(true);
+
+    JRadioButton catButton = new JRadioButton("Nike");
+    catButton.setMnemonic(KeyEvent.VK_C);
+    catButton.setActionCommand("Nike");
+
+    JRadioButton dogButton = new JRadioButton("Adidas");
+    dogButton.setMnemonic(KeyEvent.VK_D);
+    dogButton.setActionCommand("Adidas");
+
+    JRadioButton rabbitButton = new JRadioButton("Timberland");
+    rabbitButton.setMnemonic(KeyEvent.VK_R);
+    rabbitButton.setActionCommand("Timberland");
+
+    JRadioButton pigButton = new JRadioButton("Birkenstock");
+    pigButton.setMnemonic(KeyEvent.VK_P);
+    pigButton.setActionCommand("Birkenstock");
+
+    //Group the radio buttons.
+    ButtonGroup group = new ButtonGroup();
+    group.add(gucci);
+    group.add(catButton);
+    group.add(dogButton);
+    group.add(rabbitButton);
+    group.add(pigButton);
+
+    //Register a listener for the radio buttons.
+    gucci.addActionListener(this);
+    catButton.addActionListener(this);
+    dogButton.addActionListener(this);
+    rabbitButton.addActionListener(this);
+    pigButton.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    picture.setIcon(new ImageIcon("images/" 
+                                  + e.getActionCommand() 
+                                  + ".gif"));
+    }
+
+}
 
     // private void displayShoeImage(String path) {
     //     try {
@@ -67,6 +123,5 @@ public class Gui extends JFrame {
     //     }
     // }
 
-}
-
+    }
  
